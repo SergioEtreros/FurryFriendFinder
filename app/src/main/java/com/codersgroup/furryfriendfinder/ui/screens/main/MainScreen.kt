373766ea337f41a7a3c2cbe878d722143f912869
SergioEtreros <166149027+SergioEtreros@users.noreply.github.com>
2024-05-17 17:04:02 +0200

    feat: mainScreen

---
+++ app/src/main/java/com/codersgroup/furryfriendfinder/ui/screens/main/MainScreen.kt
@@ -0,0 +1,144 @@
+package com.codersgroup.furryfriendfinder.ui.screens.main
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.codersgroup.furryfriendfinder.R
+import com.codersgroup.furryfriendfinder.ui.screens.Screen
+import com.codersgroup.furryfriendfinder.ui.theme.largePadding
+import com.codersgroup.furryfriendfinder.ui.theme.smallPadding
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+  onSearch: (List<String>) -> Unit
+) {
+
+  Screen {
+    Scaffold(
+      topBar = {
+        TopAppBar(
+          title = {
+            Text(text = stringResource(R.string.app_name))
+          },
+        )
+      }
+    ) { padding ->
+
+      var textSexo by rememberSaveable { mutableStateOf("") }
+      var textEspecie by rememberSaveable { mutableStateOf("") }
+      var textRaza by rememberSaveable { mutableStateOf("") }
+      var textPeligroso by rememberSaveable { mutableStateOf("") }
+      var textLugar by rememberSaveable { mutableStateOf("") }
+      var textEsterilizado by rememberSaveable { mutableStateOf("") }
+      val modifier = Modifier.fillMaxWidth()
+
+      Column(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(padding)
+          .padding(largePadding),
+        verticalArrangement = Arrangement.spacedBy(largePadding)
+
+      ) {
+
+        SearchCombo(
+          label = stringResource(R.string.sexo),
+          optionsList = stringArrayResource(id = R.array.sexo).toList(),
+          modifier = modifier
+        ) { textSexo = it }
+
+        SearchTextField(
+          text = textEspecie,
+          label = stringResource(R.string.especie),
+          modifier = modifier
+        ) { textEspecie = it }
+
+        SearchTextField(
+          text = textRaza,
+          label = stringResource(R.string.raza),
+          modifier = modifier
+        ) { textRaza = it }
+
+        SearchCombo(
+          label = stringResource(R.string.peligroso),
+          optionsList = stringArrayResource(id = R.array.respuestasSiNo).toList(),
+          modifier = modifier
+        ) { textPeligroso = it }
+
+        SearchTextField(
+          text = textLugar,
+          label = stringResource(R.string.lugar),
+          modifier = modifier
+        ) { textLugar = it }
+
+        SearchCombo(
+          label = stringResource(R.string.esterilizado),
+          optionsList = stringArrayResource(id = R.array.respuestasSiNo).toList(),
+          modifier = modifier
+        ) { textEsterilizado = it }
+
+      }
+
+      Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(padding)
+          .padding(largePadding),
+      ) {
+        Button(
+          modifier = modifier.padding(smallPadding),
+          onClick = {
+            onSearch(listOf(
+              textSexo,
+              textEspecie,
+              textRaza,
+              textPeligroso,
+              textLugar,
+              textEsterilizado
+            ))
+          }) {
+          Text(
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.buscar)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Preview(showSystemUi = true, showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showSystemUi = true, showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun MainScreenPreview() {
+  Screen {
+    MainScreen {}
+  }
+}
